8179b06476b556d0e17f4cebd2752fc3b32c0958
safi-okz <safi90655@gmail.com>
2025-07-27 01:23:12 +0500

    setup folder staructure

--- app.vue
+++ app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold text-amber-300 underline">
-    Hello world!
-  </h1>
-  <button class="btn btn-success">
-    <Icon name="tabler:map" />
-  </button>
-<button class="btn btn-warning">Warning</button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
